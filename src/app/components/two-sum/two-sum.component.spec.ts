02cdf230d400358c6ba7702f947bc89ee80513ac
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-06 17:22:11 -0500

    components for landing page and sum of two values component

---
+++ src/app/components/two-sum/two-sum.component.spec.ts
@@ -0,0 +1,15 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TwoSumComponent } from './two-sum.component';
+
+describe('TwoSumComponent', () => {
+    let component: TwoSumComponent;
+    let fixture: ComponentFixture<TwoSumComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TwoSumComponent]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(TwoSumComponent);
