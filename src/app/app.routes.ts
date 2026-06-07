d10e0ebac2abdab0d533c4dfe4e059d0180d3b30
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-07 02:27:35 -0500

    property binding: image added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TwoSumComponent } from './components/two-sum/two-sum.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ImageContainerComponent } from './components/image-container/image-container.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },  // Default route
     { path: 'two-sum', component: TwoSumComponent },
+    { path: 'image-container', component: ImageContainerComponent },
     { path: '**', redirectTo: '' }
 ];
---
+++ src/app/components/image-container/image-container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-container.component.html',
+  styleUrl: './image-container.component.scss'
+})
+export class ImageContainerComponent {
+  imageSrc: string = 'assets/web-design.png';
+  imageAlt: string = 'Web Design Banner';
+}
