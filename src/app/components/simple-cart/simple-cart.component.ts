220579aa15b37c697d0f675b6437e971f366bcf4
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-07 16:50:29 -0500

    2-way data binding: simple cart initial setup

---
+++ src/app/components/simple-cart/simple-cart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-cart',
+  standalone: true,
+  imports: [],
+  templateUrl: './simple-cart.component.html',
+  styleUrl: './simple-cart.component.scss'
+})
+export class SimpleCartComponent {
+
+}
