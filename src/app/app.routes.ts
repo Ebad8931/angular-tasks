25265febb0f72bb69a75b9db4e83880743578d9b
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-07 15:37:32 -0500

    keyboard events: counter implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { TwoSumComponent } from './components/two-sum/two-sum.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { ImageContainerComponent } from './components/image-container/image-container.component';
+import { KeyboardCounterComponent } from './components/keyboard-counter/keyboard-counter.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },  // Default route
     { path: 'two-sum', component: TwoSumComponent },
     { path: 'image-container', component: ImageContainerComponent },
+    { path: 'keyboard-counter', component: KeyboardCounterComponent },
     { path: '**', redirectTo: '' }
 ];
