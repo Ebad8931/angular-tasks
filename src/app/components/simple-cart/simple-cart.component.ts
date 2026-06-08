52aff91ea0044549990906848959296e75ec997d
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-07 21:14:55 -0500

    2-way data binding: simple cart data binding implemented

--- src/app/components/simple-cart/simple-cart.component.ts
+++ src/app/components/simple-cart/simple-cart.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-simple-cart',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './simple-cart.component.html',
   styleUrl: './simple-cart.component.scss'
 })
 export class SimpleCartComponent {
 
+  quantity: number = 0;
+
+  pricePerItem: number = 100;
+
+  get totalPrice(): number {
+    return this.quantity * this.pricePerItem
+  }
 }
