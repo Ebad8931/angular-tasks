de64ee19a1aad52f8a75acad2b99935dd052306a
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-27 01:48:57 -0500

    ng-template & ng-container: simple profile card form added

--- src/app/components/simple-profile-card/simple-profile-card.component.ts
+++ src/app/components/simple-profile-card/simple-profile-card.component.ts
@@ -1,12 +1,22 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-simple-profile-card',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './simple-profile-card.component.html',
   styleUrl: './simple-profile-card.component.scss'
 })
 export class SimpleProfileCardComponent {
+  name: string = '';
+  email: string = '';
+  phone: string = '';
+  location: string = '';
+  qualifications = [{ school: '', degree: '', year: '' }];
 
+  addQualification() {
+    this.qualifications.push({ school: '', degree: '', year: '' });
+  }
 }
