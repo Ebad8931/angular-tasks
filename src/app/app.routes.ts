904852348120228aa5828daf6f72a8dbf403e294
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-27 00:28:34 -0500

    ng-template & ng-container: simple profile card initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { ShowModalComponent } from './components/show-modal/show-modal.component
 import { VotingAgeValidatorComponent } from './components/voting-age-validator/voting-age-validator.component';
 import { DarkModeToggleComponent } from './components/dark-mode-toggle/dark-mode-toggle.component';
 import { NameValidatorComponent } from './components/name-validator/name-validator.component';
+import { SimpleProfileCardComponent } from './components/simple-profile-card/simple-profile-card.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },  // Default route
@@ -21,5 +22,6 @@ export const routes: Routes = [
     { path: 'voter-age-checker', component: VotingAgeValidatorComponent },
     { path: 'dark-mode-toggle', component: DarkModeToggleComponent },
     { path: 'name-validator', component: NameValidatorComponent },
+    { path: 'simple-profile-card', component: SimpleProfileCardComponent },
     { path: '**', redirectTo: '' }
 ];
---
+++ src/app/components/simple-profile-card/simple-profile-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-profile-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './simple-profile-card.component.html',
+  styleUrl: './simple-profile-card.component.scss'
+})
+export class SimpleProfileCardComponent {
+
+}
