bdaba6cf0a1076cb93b7d48c204b278acc4c8cc3
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-26 23:08:29 -0500

    name validation to check if value is present using ngIf

--- src/app/components/name-validator/name-validator.component.ts
+++ src/app/components/name-validator/name-validator.component.ts
@@ -1,12 +1,14 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-name-validator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './name-validator.component.html',
   styleUrl: './name-validator.component.scss'
 })
 export class NameValidatorComponent {
-
+  fullNameValue: string = '';
 }
