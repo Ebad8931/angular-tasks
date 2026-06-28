1fe1a5555935403851e55bc73e963d2ba37a714f
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-06-27 23:17:12 -0500

    ng-template & ng-container: simple profile card form implementation

--- src/app/components/simple-profile-card/simple-profile-card.component.ts
+++ src/app/components/simple-profile-card/simple-profile-card.component.ts
@@ -1,22 +1,37 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-simple-profile-card',
   standalone: true,
-  imports: [FormsModule, NgFor],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './simple-profile-card.component.html',
   styleUrl: './simple-profile-card.component.scss'
 })
 export class SimpleProfileCardComponent {
+
+
   name: string = '';
   email: string = '';
   phone: string = '';
   location: string = '';
   qualifications = [{ school: '', degree: '', year: '' }];
 
+  showHeading: boolean = true;
+  submitted: boolean = false;
+
   addQualification() {
     this.qualifications.push({ school: '', degree: '', year: '' });
+  }
+
+  formSubmit() {
+    this.showHeading = false;
+    this.submitted = true;
+  }
+
+  formEdit() {
+    this.showHeading = true;
+    this.submitted = false
   }
 }
